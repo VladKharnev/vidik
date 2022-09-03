cc063211788a7423f7c5022d3786c49260063924
EvenTailer <vasya.vasiliy.1964@mail.ru>
2022-09-03 17:31:04 +0300

    modal

--- script/app.js
+++ script/app.js
@@ -1,82 +1,150 @@
-const API_KEY = "e0fbb552-e178-4471-9e2e-12ab59b46708"
-const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1"
-const API_URL_SEARCH = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword="
+const API_KEY = "e0fbb552-e178-4471-9e2e-12ab59b46708";
+const API_URL_POPULAR =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+const API_URL_SEARCH =
+  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 
-function menu () {
-    document.querySelector('.c-hamburger').addEventListener('click',function (e) {
-        e.preventDefault();
-       // this.classList.toggle('is-active')
-       if (this.classList.contains('is-active')) {
-        this.classList.remove ('is-active');
-        document.querySelector('#menu').classList.remove('nav-active');
-        document.body.classList.remove('body-active');
-       }else {
-        this.classList.add ('is-active');
-        document.querySelector('#menu').classList.add('nav-active');
-        document.body.classList.add('body-active');
-       }
-    })
-    }
-    menu ()
+const API_URL_MOVIE_DETAILS =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/";
 
-    getMovies(API_URL_POPULAR)
+function menu() {
+  document
+    .querySelector(".c-hamburger")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      // this.classList.toggle('is-active')
+      if (this.classList.contains("is-active")) {
+        this.classList.remove("is-active");
+        document.querySelector("#menu").classList.remove("nav-active");
+        document.body.classList.remove("body-active");
+      } else {
+        this.classList.add("is-active");
+        document.querySelector("#menu").classList.add("nav-active");
+        document.body.classList.add("body-active");
+      }
+    });
+}
+menu();
 
-    async function getMovies (url){
-        const resp = await fetch(url,{
-            headers : {
-                "Content-Type": "application/json",
-                "X-API-KEY": API_KEY,
-            },
-        });
-        const data = await resp.json();
-        showMovies(data)
-    }
-    function getClassByRate (vote) {
-        if (vote >=7) {
-           return 'green';
-        }else if (vote > 5) {
-            return 'orange';
+getMovies(API_URL_POPULAR);
 
-        }else  {
-            return 'red';
-        }
-    }
+async function getMovies(url) {
+  const resp = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const data = await resp.json();
+  showMovies(data);
+}
+function getClassByRate(vote) {
+  if (vote >= 7) {
+    return "green";
+  } else if (vote > 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
 
-    function showMovies (data){
-        const moviesEl = document.querySelector('.movies');
+function showMovies(data) {
+  const moviesEl = document.querySelector(".movies");
 
-        document.querySelector('.movies').innerHTML = "";
+  document.querySelector(".movies").innerHTML = "";
 
-        data.films.forEach(item => {
-            const movieEl = document.createElement('div')
-            movieEl.classList.add('movie')
-            movieEl.innerHTML = `
+  data.films.forEach((item) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
             <div class="movie">
             <img src="${item.posterUrl}">
             <div class="movie__info">
                 <div class="movie__title">${item.nameRu}</div>
-                <div class="movie__category">${item.genres.map(genre => ` ${genre.genre}`)}</div>
-                <div class="movie__mark movie__mark_${getClassByRate(item.rating)}">${item.rating}</div>
+                <div class="movie__category">${item.genres.map(
+                  (genre) => ` ${genre.genre}`
+                )}</div>
+                <div class="movie__mark movie__mark_${getClassByRate(
+                  item.rating
+                )}">${item.rating}</div>
             </div>
         </div>   
-            `
-        moviesEl.appendChild(movieEl)
-        });
-    }
+            `;
+    movieEl.addEventListener('click', ()=> openModal(item.filmId))
+    moviesEl.appendChild(movieEl);
+  });
+}
+
+const form = document.querySelector("form");
+const search = document.querySelector("input");
+
+form.addEventListener("keyup", (e) => {
+  e.preventDefault();
+
+  const apiSearchUrl = ` ${API_URL_SEARCH}${search.value}`;
+
+  if (search.value) {
+    getMovies(apiSearchUrl);
+  } else {
+    getMovies(API_URL_POPULAR);
+  }
+});
+function retMain() {
+  const main = document.querySelector("h1");
+  main.addEventListener("click", () => {
+    return getMovies(API_URL_POPULAR);
+  });
+}
+retMain();
+
+// Modal
+
+const modalEl = document.querySelector(".modal");
+async function openModal(id) {
+    const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-KEY": API_KEY,
+        },
+      });
+      const data = await resp.json();
+  modalEl.classList.add("modal--show");
+  document.body.classList.add('stop-scrolling');
+
+  modalEl.innerHTML = `
+    <div class="modal__card">
+            <img class="modal__movie-backdrop" src="${data.posterUrl}" alt="">
+            <h2>
+              <span class="modal__movie-title"> ${data.nameRu}</span>
+              <span class="modal__movie-year"> ${data.year}</span>
+            </h2>
+            <ul class="modal__movie-info">
+              <div class="loader"></div>
+              <li class="modal__movie-genre">Жанр : ${data.genres.map((el)=> `<span>${el.genre}</span>`)}</li>
+              ${data.filmLength ? `<li class="modal__movie-runtime"> ${data.filmLength} мимнут</li>`: ''}
+              <li >Сайт : <a class="modal__movie-stile" href="${data.webUrl}">${data.webUrl}</a></li>
+              <li class="modal__movie-overvie">Описание : ${data.description}</li>
+            </ul>
+            <button type="button" class="modal__button-close">Закрыть</button>
+          </div>
+        </div>
+    `
+    const btnClose = document.querySelector(".modal__button-close");
+btnClose.addEventListener("click", () => closeModal());
+}
 
 
-    const form = document.querySelector('form');
-    const search = document.querySelector('input');
-
-    form.addEventListener ('keyup', (e)=> {
-        e.preventDefault();
-
-        const apiSearchUrl =` ${API_URL_SEARCH}${search.value}`
-
-        if (search.value) {
-            getMovies(apiSearchUrl) ;
-
-        }else {
-            getMovies(API_URL_POPULAR)
-        }
-    })
+function closeModal() {
+  modalEl.classList.remove("modal--show");
+  document.body.classList.remove('stop-scrolling');
+}
+window.addEventListener("click", (e) => {
+  if (e.target === modalEl) {
+    closeModal();
+  }
+});
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+});
